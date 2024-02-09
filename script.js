63ddb5dd7da5cac3fe012701ec4e7debf13817cf
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-02-09 15:47:37 +0200

    flip

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var cards = document.querySelectorAll('.memory-card');
 
-function flip(){
+function flipCard(){
     console.log('flipping');
+    this.classList.toggle('flip');    
 }
 
 //add listner to cards elements
 cards.forEach(function(card){
-    card.addEventListener('click',flip);
+    card.addEventListener('click',flipCard);
 });
