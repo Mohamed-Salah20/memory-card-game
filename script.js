34d45316957448f30ac36e1ba066e2b57c301f1b
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-02-10 09:47:30 +0200

    adjusting flipCard function logic

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var cards = document.querySelectorAll('.memory-card');
 
+
+
+var isFlippedCardFirst = false;
+var firstCard;
+var secondCard;
+
+
 function flipCard(){
-    console.log('flipping');
-    this.classList.toggle('flip');    
+    this.classList.add('flip');
+    if(!isFlippedCardFirst) //In case there is no card flipped
+    {//first click
+
+        isFlippedCardFirst = true;
+        firstCard = this; //now i have the div of the first card clicked
+
+        console.log(isFlippedCardFirst,firstCard);
+    }else{//second click
+        
+        isFlippedCardFirst = false;
+        secondCard = this; //now i have the div of the second card clicked
+    
+        console.log(firstCard,secondCard);
+    }
+
 }
 
 //add listner to cards elements
