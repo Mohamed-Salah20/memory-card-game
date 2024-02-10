7a1495b61e529c204b55aa48562e5676e61754b1
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-02-10 10:38:56 +0200

    lockboard

--- script.js
+++ script.js
@@ -25,35 +25,22 @@ function flipCard(){
         console.log(firstCard,secondCard);
 
         //matching logic
-        checkForMatch();
+        if(firstCard.dataset.framework === secondCard.dataset.framework) //do the cards match? *using html data-* attribute*
+        {
+            //prevent them from being clicked again if they match by removing the click event listner
+            firstCard.removeEventListener('click',flipCard);
+            secondCard.removeEventListener('click',flipCard);
+        }else{ //not matching
+            setTimeout(function (){ //to prevent removing flip class of second card too early
+                //removing flip class from both cards
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            },500);
+        }
     }
 
 }
 
-function checkForMatch(){
-    //matching logic
-    if(firstCard.dataset.framework === secondCard.dataset.framework) //do the cards match? *using html data-* attribute*
-    {
-        //prevent them from being clicked again if they match by removing the click event listner
-        disableCards();
-    }else{ //not matching
-        unFlipCards(); //disable
-    }
-}
-
-function disableCards(){
-    //prevent them from being clicked again if they match by removing the click event listner
-    firstCard.removeEventListener('click',flipCard);
-    secondCard.removeEventListener('click',flipCard);
-}
-
-function unFlipCards(){
-    setTimeout(function (){ //to prevent removing flip class of second card too early
-        //removing flip class from both cards
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-    },500);
-}
 //add listner to cards elements
 cards.forEach(function(card){
     card.addEventListener('click',flipCard);
