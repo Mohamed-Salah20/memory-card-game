b27c556d5ae30fb10de83799a97aa22519075b5d
Mohamed-Salah20 <mohamed.salah.ms2000@gmail.com>
2024-02-10 10:29:06 +0200

    lockboard

--- script.js
+++ script.js
@@ -6,9 +6,12 @@ var isFlippedCardFirst = false;
 var firstCard;
 var secondCard;
 
+var lockBoard = false;
 
 function flipCard(){
     console.log('flipping function');
+    if (lockBoard) return;
+
     this.classList.add('flip');
     if(!isFlippedCardFirst) //In case there is no card flipped
     {//first click
@@ -25,22 +28,38 @@ function flipCard(){
         console.log(firstCard,secondCard);
 
         //matching logic
-        if(firstCard.dataset.framework === secondCard.dataset.framework) //do the cards match? *using html data-* attribute*
-        {
-            //prevent them from being clicked again if they match by removing the click event listner
-            firstCard.removeEventListener('click',flipCard);
-            secondCard.removeEventListener('click',flipCard);
-        }else{ //not matching
-            setTimeout(function (){ //to prevent removing flip class of second card too early
-                //removing flip class from both cards
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            },500);
-        }
+        checkForMatch();
     }
 
 }
 
+function checkForMatch(){
+    //matching logic
+    if(firstCard.dataset.framework === secondCard.dataset.framework) //do the cards match? *using html data-* attribute*
+    {
+        //prevent them from being clicked again if they match by removing the click event listner
+        disableCards();
+    }else{ //not matching
+        unFlipCards(); //disable
+    }
+}
+
+function disableCards(){
+    //prevent them from being clicked again if they match by removing the click event listner
+    firstCard.removeEventListener('click',flipCard);
+    secondCard.removeEventListener('click',flipCard);
+}
+
+function unFlipCards(){
+    lockBoard = true;
+    setTimeout(function (){ //to prevent removing flip class of second card too early
+        //removing flip class from both cards
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        lockBoard = false;
+    },500);
+}
 //add listner to cards elements
 cards.forEach(function(card){
     card.addEventListener('click',flipCard);
